fa9feae3cdb3bd17074f8f2bf4f9be68171c661e
Fy1717 <frknyldz8489@gmail.com>
2022-11-24 20:44:39 +0300

    questions and homeworks added

---
+++ homeworks.py
@@ -0,0 +1,21 @@
+# 1 - İsminizi ve okudunuz bölümü bir string değişkeni içinde söyleyin.
+#       (Örneğin: "Furkan Yıldız. Bilgisayar Mühendisliği.")
+#       Daha sonra bu yazıdaki ünlü harfleri uygun metodla silip ekrana son halini yazdırın 
+
+
+
+
+
+# 2 - Kendinize ait 3 özelliği bir "Dictionary" değişkende tanımlayın. 
+#      (Örneğin: 'name', 'surname', 'age')
+#      Daha sonra her bir özelliği alt alta ekrana yazdırın.
+#      Sadece 1 defa print fonksiyonunu kullanabilirsiniz.
+
+
+
+
+
+# 3 - Kullanıcıdan ismini ve yaşını isteyin (input)
+#     Kullanıcı ile aranızdaki yaş farkını anlamlı bir şekilde ekrana yazdırın
+#     (Örneğin: "Enes benden 3 yaş küçüktür")
+
