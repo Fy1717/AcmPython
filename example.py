6b80e212ce346db51c6782494097a689d7b7b91c
Fy1717 <frknyldz8489@gmail.com>
2022-11-25 19:58:53 +0300

    example file added

---
+++ example.py
@@ -0,0 +1,31 @@
+''' for number in range(0, 10, 2):
+    print("Number is : ", number) '''
+
+''' user1 = {"name": "Furkan", "job": "CE", "age": 25}
+user2 = {"name": "Enes", "job": "EE", "age": 23}
+user3 = {"name": "Mustafa", "job": "CE", "age": 24}
+
+userList = [user1, user2, user3]
+
+
+sumOfAges = 0
+for user in userList:
+    #print("NAME : ", user["name"], "  JOB : ", user["job"], " AGE : ", user["age"])
+    
+    sumOfAges = sumOfAges + user["age"]
+
+print("AVERAGE OF AGES : ", sumOfAges / (len(userList))) '''
+
+''' for i in range(6):
+    print("Furkan") '''
+
+
+
+
+
+
+
+
+
+#print("USER : ", user1)
+#print("USER NAME : ", user1["name"], " ", user2["job"], " ", user3["age"])
