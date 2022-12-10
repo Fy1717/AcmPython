80e9c927a9f33a0c4d57a90cc4aa7218a43aeebc
Fy1717 <frknyldz8489@gmail.com>
2022-12-10 12:08:12 +0300

    oop sample added

--- example.py
+++ example.py
@@ -21,11 +21,53 @@ print("AVERAGE OF AGES : ", sumOfAges / (len(userList))) '''
 
 
 
-
-
-
-
-
-
 #print("USER : ", user1)
 #print("USER NAME : ", user1["name"], " ", user2["job"], " ", user3["age"])
+
+from datetime import date
+
+class User:
+    todays_date = date.today()
+
+    def __init__(self, name, job, birthyear):
+        self.name = name
+        self.job = job
+        self.birthyear = birthyear
+        
+        
+    def calcAge(self):
+        print(todays_date.year - self.birthyear)
+        
+        
+user1 = User("Furkan", "Software Engineer", 1997)
+
+user1.calcAge()
+
+
+
+
+
+
+
+
+
+
+user2 = User("Deniz", "Student", 1999)
+user3 = User("Melih", "Student", 2005)
+
+#print(todays_date.year - user1.birthyear)
+''' userList = [user1, user2, user3]
+ '''
+''' for user in userList:
+    print("NAME: ", user.name, " JOB: ", user.job, " AGE: ", user.age) '''
+
+
+
+
+#print(userList[0].name)
+
+#print(userList)
+
+''' print(user1.name)
+print(user2.name)
+ '''
